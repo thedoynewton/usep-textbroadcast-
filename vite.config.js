58131490859435e7f7b02caa440f734f87b5afa8
USePTBC <usepsephi_tbs@usep.edu.ph>
2024-11-26 18:29:10 +0800

    fix errors

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,24 @@
+
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import fs from 'fs';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
+            // input: [
+            //     'resources/css/app.css',
+            //     'resources/js/app.js',
+            //     'resources/js/dashboardFilter.js',
+            //     'resources/js/generateReport.js',
+            // ],
             input: [
                 'resources/css/app.css',
-                'resources/js/app.js',
+                ...fs
+                    .readdirSync(path.resolve(__dirname, 'resources/js'))
+                    .filter(file => file.endsWith('.js'))
+                    .map(file => `resources/js/${file}`),
             ],
             refresh: true,
         }),
